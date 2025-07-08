808d9328b8b43925f10fd20f648c8d9ff7f9e890
Nathaniel Strange <ns2685@bath.ac.uk>
2025-07-08 11:18:21 +0100

    Pushing with altered script

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ DATA_FILES = []
 
 OPTIONS = {
     'argv_emulation': True,
-    'includes': ['Bio', 'numpy'],  # Use 'includes' instead of 'packages'
+    'includes': ['Bio', 'numpy', 'tkinter'],  # Use 'includes' instead of 'packages'
     'packages': [],  # Leave empty unless you're bundling folders/modules
-    'excludes': ['tkinter'],  # Exclude if not used (py2app might try to include)
     'iconfile': 'icon.icns',  # Optional: only if you have a custom icon
     'plist': {
         'CFBundleName': 'MyBioApp',
