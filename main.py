2b368d2cbb7359ebea1aabf8db267368f753a3b0
Nathaniel Strange <ns2685@bath.ac.uk>
2025-07-08 11:36:55 +0100

    Fixing hanging

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from gui.gui_utils import set_connection
 from utils import get_db_path
 import tkinter as tk
 import sqlite3
+import sys
 
 def main():
+    if '--test' in sys.argv:
+        print("Test run: app starts correctly.")
+        sys.exit(0)
 
     set_connection(sqlite3.connect(get_db_path()))
 
