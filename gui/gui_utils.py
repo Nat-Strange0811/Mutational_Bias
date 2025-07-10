70f0b40df7c966e6c895bff17e274b96c870d235
Nathaniel Strange <ns2685@bath.ac.uk>
2025-07-10 20:26:58 +0100

    Adding if to not destroy menu

--- gui/gui_utils.py
+++ gui/gui_utils.py
@@ -11,7 +11,8 @@ import sys
 
 def clear_window(window):
     for widget in window.winfo_children():
-        widget.destroy()
+        if not isinstance(widget, tk.Menu):
+            widget.destroy()
     if hasattr(window, 'main_menu'):
         window.main_menu.delete(0, 'end')  # Clear the menu bar if it exists
 
