197d37d52493b6bb5de1cf65a7f023fdd24efb7a
Nathaniel Strange <ns2685@bath.ac.uk>
2025-07-17 11:37:56 +0100

    Fixing setup

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ DATA_FILES = ['Database/CpG_data.db']
 
 OPTIONS = {
     'argv_emulation': True,
-    'includes': ['tkinter', 'sqlite3', 'os', 'shutil', 'pathlib', 'psygog2', 'decimal'],
-    'packages': ['Bio', 'numpy', 'appdirs'],
+    'includes': ['tkinter', 'sqlite3', 'os', 'shutil', 'pathlib', 'psycopg2', 'psycopg2._psycopg', 'decimal'],
+    'packages': ['Bio', 'numpy', 'appdirs', 'psycopg2'],
     'resources': ['Database'],
-    'compressed' : False,
+    'compressed': False,
     'plist': {
         'CFBundleName': 'Mutational Bias',
         'CFBundleDisplayName': 'Mutational Bias',
